7445e9f955545eb4a17eaa68c1cd56c4a65aca21
jorgtunubala <jorgetunubala@unicauca.edu.co>
2024-11-07 12:05:42 -0500

    Deshabilita CSRF para pruebas

--- src/main/java/com/maestria/gestion/autenticacion/usuarios/configuration/SecurityConfig.java
+++ src/main/java/com/maestria/gestion/autenticacion/usuarios/configuration/SecurityConfig.java
@@ -1,9 +1,16 @@
 package com.maestria.gestion.autenticacion.usuarios.configuration;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -14,9 +21,23 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // Deshabilita CSRF para pruebas
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Configurar CORS
                 .authorizeRequests(requests -> requests
                         .requestMatchers("/api/auth/google").permitAll() // Permitir el acceso sin autenticación a este endpoint
                         .anyRequest().authenticated()); // Requiere autenticación para cualquier otro endpoint
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:4200")); // Cambia al dominio del front-end
+        config.setAllowedMethods(List.of("GET", "POST"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 }
